4ff34f2dddb4716ab0842bba753b2be1e12c0f9f
Hyacinth Augusting <augustinehyacinth3@gmail.com>
2025-08-28 18:40:22 +0100

    dark mode now waorking, ready for depolyment

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,16 @@ import { useState, useEffect } from "react";
 import ActivityBar from "./Components/ActivityBar";
 import Card from "./Components/Card";
 import NavBar from "./Components/NavBar";
-import DB from "./Firebase/server";
-import { ref, onValue } from "firebase/database";
+import AppData from "./data.json";
+import type { Extension } from "./Types";
 
 const App = () => {
   const [filter, setFilter] = useState("all");
-  const [apps, setApps] = useState<any[]>([]);
+  const [apps, setApps] = useState<Extension[]>([]);
 
   useEffect(() => {
-    const extensionsRef = ref(DB, "extensions");
-    onValue(extensionsRef, (snapshot) => {
-      if (snapshot.exists()) {
-        const data = snapshot.val();
-        const appsArray = Object.values(data);
-        setApps(appsArray);
-      }
-    });
+    sessionStorage.setItem("extensions", JSON.stringify(AppData));
+    setApps(JSON.parse(sessionStorage.getItem("extensions")!) as Extension[]);
   }, []);
 
   const filteredData = apps.filter((app) => {
@@ -27,11 +21,10 @@ const App = () => {
   });
 
   return (
-    <div className="font-Nato-Sans text-lg pt-5 pb-20 bg-neutral-900 min-h-dvh text-neutral-0">
+    <div className="font-Nato-Sans text-lg pt-5 pb-10 bg-neutral-900 dark:bg-neutral-200 min-h-dvh text-neutral-0">
       <div className="max-w-7xl w-full mx-auto px-2 overflow-x-hidden md:px-4">
         <div className="flex flex-col gap-10">
           <NavBar />
-
           <div>
             <ActivityBar active={filter} setActive={setFilter} />
             <div className="grid grid-cols-12 gap-2 mt-6">
@@ -42,10 +35,31 @@ const App = () => {
                   cardTitle={app.name}
                   logo={app.logo}
                   isActive={app.isActive}
+                  apps={apps}
+                  setApps={setApps}
                 />
               ))}
             </div>
           </div>
+        </div>
+
+        <div className="mt-10 text-center md:text-2xl text-xl px-4 md:px-0 dark:text-neutral-700 text-neutral-100">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            className="underline underline-offset-4 text-red-400 transition-all duration-300 ease-in-out"
+          >
+            Frontend Mentor.
+          </a>{" "}
+          Coded by{" "}
+          <a
+            href="https://github.com/Hyacinthaugustine/frontend-browser-extension"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-red-400 hover:underline hover:underline-offset-4 hover:transition-all"
+          >
+            Hyacinth Augustine.
+          </a>
         </div>
       </div>
     </div>
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,35 +1,46 @@
 import { useState } from "react";
 import Button from "./Button";
 import ToggleBtn from "./ToggleBtn";
-import DB from "../Firebase/server";
-import { ref, update, remove } from "firebase/database";
+import type { Extension } from "../Types";
 
 interface CardPProp {
   cardTitle: string;
   cardText: string;
   logo: string;
   isActive: boolean;
+  apps: Extension[];
+  setApps: React.Dispatch<React.SetStateAction<Extension[]>>;
 }
 
-const Card = ({ logo, cardText, cardTitle, isActive }: CardPProp) => {
+const Card = ({
+  logo,
+  cardText,
+  cardTitle,
+  isActive,
+  apps,
+  setApps,
+}: CardPProp) => {
   const [showConfirm, setShowConfirm] = useState(false);
 
   const handleToggle = () => {
-    const cardRef = ref(DB, `extensions/${cardTitle}`);
-    update(cardRef, { isActive: !isActive });
+    const updated = apps.map((app) =>
+      app.name === cardTitle ? { ...app, isActive: !app.isActive } : app
+    );
+    setApps(updated);
+    sessionStorage.setItem("extensions", JSON.stringify(updated));
   };
 
   const handleRemove = () => {
-    const cardRef = ref(DB, `extensions/${cardTitle}`);
-    remove(cardRef);
+    const updated = apps.filter((app) => app.name !== cardTitle);
+    setApps(updated);
+    sessionStorage.setItem("extensions", JSON.stringify(updated));
     setShowConfirm(false);
   };
 
   return (
     <div className="lg:col-span-4 md:col-span-6 col-span-12 md:rounded-xl rounded-2xl border border-neutral-600 bg-neutral-800 p-4 flex flex-col justify-between gap-10 dark:bg-neutral-50 dark:border-neutral-300">
       <div className="flex gap-4 items-start">
-        <img src={logo} alt={`${cardText} logo`} />
-
+        <img src={logo} alt={`${cardTitle} logo`} />
         <div>
           <h3 className="text-xl dark:text-neutral-800 dark:font-medium font-medium">
             {cardTitle}
@@ -47,20 +58,19 @@ const Card = ({ logo, cardText, cardTitle, isActive }: CardPProp) => {
 
       {showConfirm && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-          <div className="bg-white dark:bg-neutral-800 rounded-xl text-center shadow-lg p-6 max-w-2xl w-full border-red-500 border-3">
+          <div className="bg-white dark:bg-neutral-800 rounded-xl text-center shadow-lg p-6 max-w-2xl w-full border-red-500 border-3 m-3 md:m-0">
             <h2 className="text-xl font-semibold text-neutral-800 dark:text-neutral-100">
               Confirm Deletion
             </h2>
             <p className="text-neutral-600 dark:text-neutral-300 mb-6">
               Are you sure you want to remove{" "}
-              <strong className="text-neutral-0">{cardTitle}</strong> extension?
+              <strong className="text-red-500">{cardTitle}</strong> extension?
             </p>
             <div className="flex justify-center items-center gap-4">
               <Button
                 onClick={() => setShowConfirm(false)}
                 ButtonText="Cancel"
               />
-
               <Button onClick={handleRemove} ButtonText="Remove" />
             </div>
           </div>
